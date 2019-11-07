17f6385c209638caa195fbe7f3e3519232cd0ce2
Gerard Royo <gerard.royo@mataro.epiaedu.cat>
2019-11-07 17:03:28 +0100

    wallpaper primer layout ready

--- app/src/main/java/com/seriouspoke/MainActivity.java
+++ app/src/main/java/com/seriouspoke/MainActivity.java
@@ -3,6 +3,8 @@ package com.seriouspoke;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // deshabilita el titol
+        getSupportActionBar().hide();
+
+        // deshabilita la barra de notificacions
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
