06e0516e0b8aeb435dd11cb3c536e346e4d3e29e
Gerard Royo <gerard.royo@mataro.epiaedu.cat>
2019-11-12 21:11:53 +0100

    introduint dades pokemon

--- app/src/main/java/com/seriouspoke/ListPrincipal.java
+++ app/src/main/java/com/seriouspoke/ListPrincipal.java
@@ -2,6 +2,7 @@ package com.seriouspoke;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,8 +18,41 @@ public class ListPrincipal extends AppCompatActivity {
 
     private cPokimon[] Pokemons =
             new cPokimon[] {
-                    new cPokimon("001", "Treecko", "Planta", "", "@drawable/img_pokemon/treecko"),
-                    new cPokimon("002", "Grovyle", "Planta", "", "@drawable/img_pokemon/grovyle")
+                    new cPokimon("001", "Treecko", "Planta", ""),
+                    new cPokimon("002", "Grovyle", "Planta", ""),
+                    new cPokimon("003", "Sceptile", "Planta", ""),
+                    new cPokimon("004", "Torchic", "Fuego", ""),
+                    new cPokimon("005", "Combusken", "Fuego", "Lucha"),
+                    new cPokimon("006", "Blaziken", "Fuego", "Lucha"),
+                    new cPokimon("007", "Mudkip", "Agua", ""),
+                    new cPokimon("008", "Marshtomp", "Agua", "Tierra"),
+                    new cPokimon("009", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("010", "Poochyena", "Siniestro", ""),
+                    new cPokimon("011", "Mightyena", "Siniestro", ""),
+                    new cPokimon("012", "Zigzagoon", "Normal", ""),
+                    new cPokimon("013", "Linoone", "Normal", ""),
+                    new cPokimon("014", "Wurmple", "Bicho", ""),
+                    new cPokimon("015", "Silcoon", "Bicho", ""),
+                    new cPokimon("016", "Beautifly", "Bicho", "Volador"),
+                    new cPokimon("017", "Cascoon", "Bicho", ""),
+                    new cPokimon("018", "Dustox", "Bicho", "Veneno"),
+                    new cPokimon("019", "Lotad", "Agua", "Planta"),
+                    new cPokimon("020", "Lombre", "Agua", "Planta"),
+                    new cPokimon("021", "Ludicolo", "Agua", "Planta"),
+                    new cPokimon("022", "Seedot", "Planta", ""),
+                    new cPokimon("023", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("024", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("025", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("026", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("027", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("028", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("029", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("030", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("031", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("032", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("033", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("034", "Swampert", "Agua", "Tierra"),
+                    new cPokimon("035", "Swampert", "Agua", "Tierra")
             };
 
     @Override
@@ -59,14 +93,97 @@ class AdaptadorPokemons extends ArrayAdapter<cPokimon> {
 
         cPokimon pokemon = (cPokimon) getItem(position);
 
-        ImageView img = (ImageView)item.findViewById(R.id.imgPokemon);
-        img.setImageResource(R.drawable.treecko);
-
         TextView tv = (TextView)item.findViewById(R.id.tvNumPokedex);
         tv.setText(pokemon.getNumPokedex());
 
         tv = (TextView)item.findViewById(R.id.tvNombre);
         tv.setText(pokemon.getNombre());
+
+        ImageView img = (ImageView)item.findViewById(R.id.imgPokemon);
+
+        switch (pokemon.getNombre()) {
+            case "Treecko":
+                img.setImageResource(R.drawable.treecko);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Sceptile":
+                img.setImageResource(R.drawable.sceptile);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+            case "Grovyle":
+                img.setImageResource(R.drawable.grovyle);
+                break;
+        }
 
         tv = (TextView)item.findViewById(R.id.tvTipo1);
         hexColor = escollirColor(pokemon.getTipo1());
--- app/src/main/java/com/seriouspoke/cPokimon.java
+++ app/src/main/java/com/seriouspoke/cPokimon.java
@@ -6,7 +6,6 @@ public class cPokimon {
     private String nombre;
     private String tipo1;
     private String tipo2 = "";
-    private String imgPokemon;
     //private String habilidad;
     //private String habOculta;
     //private double peso;
@@ -15,12 +14,11 @@ public class cPokimon {
     //private String generoF;
     //private String habitat;
 
-    public cPokimon (String sNumPokedex, String sNombre, String sTipo1, String sTipo2, String sImgPokemon) {
+    public cPokimon (String sNumPokedex, String sNombre, String sTipo1, String sTipo2) {
         numPokedex = sNumPokedex;
         nombre = sNombre;
         tipo1 = sTipo1;
         tipo2 = sTipo2;
-        imgPokemon = sImgPokemon;
     }
 
     public String getNumPokedex() { return numPokedex; }
@@ -38,8 +36,4 @@ public class cPokimon {
     public String getTipo2() { return tipo2; }
 
     public void setTipo2(String tipo2) { this.tipo2 = tipo2; }
-
-    public String getImgPokemon() { return imgPokemon; }
-
-    public void setImgPokemon(String imgPokemon) { this.imgPokemon = imgPokemon; }
 }
