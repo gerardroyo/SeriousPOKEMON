659d2ab6a153fb80ac2b567dca47af02ffefc298
gerardroyo <gerard.royo@mataro.epiaedu.cat>
2019-11-19 13:30:30 +0100

    En pracess d'introducció de dades especifiques dels pokemons

--- app/src/main/java/com/seriouspoke/cPokimon.java
+++ app/src/main/java/com/seriouspoke/cPokimon.java
@@ -6,19 +6,26 @@ public class cPokimon {
     private String nombre;
     private String tipo1;
     private String tipo2 = "";
-    //private String habilidad;
-    //private String habOculta;
-    //private double peso;
-    //private double altura;
-    //private String generoM;
-    //private String generoF;
-    //private String habitat;
+    private String habilidad;
+    private String habOculta;
+    private double peso;
+    private double altura;
+    private String generoM;
+    private String generoF;
+    private String habitat;
 
-    public cPokimon (String sNumPokedex, String sNombre, String sTipo1, String sTipo2) {
+    public cPokimon(String sNumPokedex, String sNombre, String sTipo1, String sTipo2, String sHabilidad, String sHabOculta, double sPeso, double sAltura, String sGeneroM, String sGeneroF, String sHabitat) {
         numPokedex = sNumPokedex;
         nombre = sNombre;
         tipo1 = sTipo1;
         tipo2 = sTipo2;
+        habilidad = sHabilidad;
+        habOculta = sHabOculta;
+        peso = sPeso;
+        altura = sAltura;
+        generoM = sGeneroM;
+        generoF = sGeneroF;
+        habitat = sHabitat;
     }
 
     public String getNumPokedex() { return numPokedex; }
@@ -36,4 +43,32 @@ public class cPokimon {
     public String getTipo2() { return tipo2; }
 
     public void setTipo2(String tipo2) { this.tipo2 = tipo2; }
+
+    public String getHabilidad() { return habilidad; }
+
+    public void setHabilidad(String habilidad) { this.habilidad = habilidad; }
+
+    public String getHabOculta() { return habOculta; }
+
+    public void setHabOculta(String habOculta) { this.habOculta = habOculta; }
+
+    public double getPeso() { return peso; }
+
+    public void setPeso(double peso) { this.peso = peso; }
+
+    public double getAltura() { return altura; }
+
+    public void setAltura(double altura) { this.altura = altura; }
+
+    public String getGeneroM() { return generoM; }
+
+    public void setGeneroM(String generoM) { this.generoM = generoM; }
+
+    public String getGeneroF() { return generoF; }
+
+    public void setGeneroF(String generoF) { this.generoF = generoF; }
+
+    public String getHabitat() { return habitat; }
+
+    public void setHabitat(String habitat) { this.habitat = habitat; }
 }
